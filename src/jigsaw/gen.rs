2d4f90e18d6995898c27f43796b6caca80703bf0
Nertsal <sasha.kudasov04@gmail.com>
2023-01-07 20:45:48 +0300

    Better pattern

--- src/jigsaw/gen.rs
+++ src/jigsaw/gen.rs
@@ -3,10 +3,7 @@ use super::*;
 type Mesh = Vec<[JigsawVertex; 3]>;
 
 pub fn generate_jigsaw(ugli: &Ugli, size: Vec2<f32>, pieces: Vec2<usize>) -> Vec<JigsawMesh> {
-    finalize_meshes(
-        ugli,
-        scale(size, triangulate(pieces, randomize(uniform(pieces)))),
-    )
+    finalize_meshes(ugli, scale(size, triangulate(pieces, jigsaw(pieces))))
 }
 
 fn finalize_meshes(ugli: &Ugli, meshes: Vec<Mesh>) -> Vec<JigsawMesh> {
@@ -18,27 +15,79 @@ fn finalize_meshes(ugli: &Ugli, meshes: Vec<Mesh>) -> Vec<JigsawMesh> {
 
 type Polygon = Vec<Vec2<f32>>;
 
-fn uniform(size: Vec2<usize>) -> Vec<Polygon> {
-    // TODO: actual jigsaw - not just rectangles
+fn jigsaw(size: Vec2<usize>) -> Vec<Polygon> {
     let tile_size = vec2(1.0, 1.0) / size.map(|x| x as f32);
-    (0..size.y)
+    let mut jigsaw: Vec<[Vec<Vec2<f32>>; 4]> = (0..size.y)
         .flat_map(|y| {
             (0..size.x).map(move |x| {
                 let pos = vec2(x as f32, y as f32) * tile_size;
-                vec![
-                    pos,
-                    pos + vec2(tile_size.x, 0.0),
-                    pos + tile_size,
-                    pos + vec2(0.0, tile_size.y),
+                [
+                    vec![pos],
+                    vec![pos + vec2(tile_size.x, 0.0)],
+                    vec![pos + tile_size],
+                    vec![pos + vec2(0.0, tile_size.y)],
                 ]
             })
         })
-        .collect()
-}
+        .collect();
 
-fn randomize(polygons: Vec<Polygon>) -> Vec<Polygon> {
-    // TODO
-    polygons
+    let vertical_edges = size.y * (size.x - 1);
+    let edges_count = vertical_edges + size.x * (size.y - 1);
+    let mut rng = geng::prelude::thread_rng();
+    let knob = vec![
+        vec2(0.0, 0.0275),
+        vec2(-0.058, 0.144),
+        vec2(0.0, 0.2725),
+        vec2(0.1, 0.3081),
+        vec2(0.2, 0.2725),
+        vec2(0.258, 0.144),
+        vec2(0.2, 0.0275),
+    ];
+    let mut edges: Vec<Vec<Vec2<f32>>> = (0..edges_count)
+        .map(|i| {
+            let t = i as f32 / (edges_count as f32 - 1.0);
+            let start = 0.2 + t * 0.4;
+            let end = start + 0.2;
+            itertools::chain![
+                [vec2(start, 0.0)],
+                knob.iter().map(|v| *v + vec2(start, 0.0)),
+                [vec2(end, 0.0)],
+            ]
+            .collect()
+        })
+        .collect();
+    edges.shuffle(&mut rng);
+
+    for (i, mut edge) in edges.into_iter().enumerate() {
+        let vertical = i < vertical_edges;
+        let tile = if vertical {
+            i / size.y + i % size.y * size.x
+        } else {
+            i - vertical_edges
+        };
+        let pos = vec2(tile % size.x, tile / size.x).map(|x| x as f32) * tile_size;
+        let other = if vertical { tile + 1 } else { tile + size.x };
+        let scale = if vertical { tile_size.y } else { tile_size.x };
+        if vertical {
+            edge.iter_mut()
+                .for_each(|v| *v = v.rotate_90() * scale + pos + vec2(tile_size.x, 0.0));
+        } else {
+            edge.iter_mut()
+                .for_each(|v| *v = *v * scale + pos + vec2(0.0, tile_size.y));
+        }
+        if vertical {
+            jigsaw[other][3].extend(edge.iter().rev().copied());
+            jigsaw[tile][1].extend(edge);
+        } else {
+            jigsaw[tile][2].extend(edge.iter().rev().copied());
+            jigsaw[other][0].extend(edge);
+        }
+    }
+
+    jigsaw
+        .into_iter()
+        .map(|sides| sides.into_iter().flatten().collect())
+        .collect()
 }
 
 fn triangulate(pieces: Vec2<usize>, polygons: Vec<Polygon>) -> Vec<Mesh> {
