d1f3388b3432f2e4e5b7a38a9108360ca4e0021d
kuviman <kuviman@gmail.com>
2023-01-09 14:16:23 +0400

    reduce interpolation time

--- src/interpolation.rs
+++ src/interpolation.rs
@@ -44,7 +44,7 @@ impl Zero for Vec3<f32> {
     };
 }
 
-const INTERPOLATION_TIME: f32 = 0.5;
+const INTERPOLATION_TIME: f32 = 0.2;
 
 impl<T: Mul<f32, Output = T> + Add<Output = T> + Sub<Output = T> + Copy + Zero> Interpolated<T> {
     pub fn new(p: T, v: T) -> Self {
